cb1f0b14dfacfff558e04c8dfa6bc0d8c9326b62
Agung Riswanto <ggriswanto02@gmail.com>
2025-12-30 13:34:44 +0700

    feat: jwt auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+export function authenticate(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.sendStatus(401);
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    req.user = payload;
+    next();
+  } catch {
+    return res.sendStatus(403);
+  }
+}
